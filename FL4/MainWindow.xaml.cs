62b68bae084d3dd9c150ea961e23659245c9bd6b
Jonathan <jojo1130@student.miun.se>
2019-11-08 20:18:06 +0100

    Add project files.

---
+++ FL4/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FL4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculateBmi_Click(object sender, RoutedEventArgs e)
+        {
+            double height = double.Parse(txtHeight.Text), mass = double.Parse(txtMass.Text);
+            double bmi;
+            bmi = mass / (height * height);
+            txtResult.Text = Math.Round(bmi, 2).ToString();
+
+            bmi = 24.5;
+            int value = (int)bmi; // cast, låssas att det är en integer 
+
+            if (bmi < 18.5)
+            {
+
+            }
+            else if (bmi >= 18.5 && bmi <= 25)
+            {
+                // normal
+            }
+            else
+            {
+                // Övervikt
+            }
+        }
+    }
+}
---
+++ Yatzy/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Yatzy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRollDices_Click(object sender, RoutedEventArgs e)
+        {
+            int dieOne, dieTwo, dieThree, dieFour, dieFive, totalSum, bonus;
+            // uppdrag
+            // kasta en tärning och presentera resultatet
+
+
+            dieOne = random.Next(1, 7);
+            dieTwo = random.Next(1, 7);
+            dieThree = random.Next(1, 7);
+            dieFour = random.Next(1, 7);
+            dieFive = random.Next(1, 7);
+
+            lbldieOne.Content = dieOne;
+            lbldieTwo.Content = dieTwo;
+            lbldieThree.Content = dieThree;
+            lbldieFour.Content = dieFour;
+            lbldieFive.Content = dieFive;
+
+            totalSum = dieOne + dieTwo + dieThree + dieFour + dieFive;
+
+            if (totalSum >= 63)
+            {
+                bonus = 50;
+                totalSum += bonus;
+
+            }
+            else
+            {
+                bonus = 0;
+            }
+            txtBonus.Text = bonus.ToString();
+            // Alt piltangent för att flytta rad
+            txtTotal.Text = totalSum.ToString();
+        }
+    }
+}
