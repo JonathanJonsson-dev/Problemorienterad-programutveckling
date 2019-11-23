6e4a7a6fab7a0f03ef76859910ad1022c016d769
Jonathan <jojo1130@student.miun.se>
2019-11-23 19:15:53 +0100

    Yatzy

--- Yatzy/GameEngine.cs
+++ Yatzy/GameEngine.cs
@@ -13,12 +13,125 @@ namespace Yatzy
         // private och public
 
         // uppdrag: gör en metod som kastar en tärning
+
+        // skapa slumpgenerator
+        private Random random = new Random();
+
+        // publika properties
+        /// <summary>
+        /// Game category one
+        /// </summary>
+        public int Ones { get; set; }
+        public int Twos { get; set; }
+        public int Threes { get; set; }
+        public int Fours { get; set; }
+        public int Fives { get; set; }
+        public int Sixes { get; set; }
+        public int Bonus { get; set; }
+        public int Total { get; set; }
+
+
+
+
+
+
+        /// <summary>
+        /// Check if a score is valid for this category
+        /// </summary>
+        /// <returns></returns>
+        public bool IsValidScore(int score, int category)
+        {
+            // kollar extremvärden
+            int dicesCount = 5;
+            if (score < 0 || score > category * dicesCount)
+            {
+                return false;
+            }
+
+            else if (score % category != 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsCategoryAvailable(int category)
+        {
+            switch (category)
+            {
+                case 1:
+                    if (Ones == 0)
+                    {
+                        return true;
+                    }
+                    break;
+            }
+            return false;
+
+        }
+
+
+
+        public bool SaveScore(int score, int category)
+        {
+            // ! vänder på min if sats, alltså inte är okej
+            if (!IsValidScore(score, category))
+            {
+                return false;
+            }
+            else if (!IsCategoryAvailable(category))
+            {
+                return false;
+            }
+            else
+            {
+                // switch, samma som många if satser
+                switch (category)
+                {
+                    case 1:
+                        Ones = score;
+                        break;
+                    case 2:
+                        Twos = score;
+                        break;
+                    case 3:
+                        Threes = score;
+                        break;
+                    case 4:
+                        Fours = score;
+                        break;
+                    case 5:
+                        Fives = score;
+                        break;
+                    case 6:
+                        Sixes = score;
+                        break;
+                }
+                return true;
+            }
+
+        }
+
+
+
+
         public int RoleDie()
         {
             // instansvariabel
             Random random = new Random(); //slumpgenerator
             return random.Next(1, 7);
         }
+
+        public int[] RoleDices()
+        {
+            int[] dices = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                dices[i] = RoleDie();
+            }
+            return dices;
+        }
+
 
     }
 }
--- Yatzy/MainWindow.xaml.cs
+++ Yatzy/MainWindow.xaml.cs
@@ -21,21 +21,30 @@ namespace Yatzy
     public partial class MainWindow : Window
     {
         Random random = new Random();
+        GameEngine gameEngine;
+
+
         public MainWindow()
         {
+            // Den här koden körs endas en gång
             InitializeComponent();
+            gameEngine = new GameEngine();
         }
 
         private void btnRollDices_Click(object sender, RoutedEventArgs e)
         {
 
+            
+
+
+
+
             // skapa ett objekt/instans av GameEngine 
-
             GameEngine gameEngine = new GameEngine();
+            int score = 13;
+            int category = 3;
 
-
-
-
+            bool result = gameEngine.IsValidScore(score, category);
             int dieOne, dieTwo, dieThree, dieFour, dieFive;
             dieOne = gameEngine.RoleDie();
 
@@ -90,6 +99,21 @@ namespace Yatzy
 
             lbldieOne.Content = dices[0];
             lbldieTwo.Content = dices[1];
+
+        }
+
+        private void BtnSaveScore_Click(object sender, RoutedEventArgs e)
+        {
+            
+            int score = 13;
+            //bool result = gameEngine.IsValidScore(score, 3);
+            bool result = gameEngine.SaveScore(score, 3);
+
+            gameEngine.Ones = 11;
+            score = gameEngine.Ones;
+            
+            //SaveScore saveScore = new SaveScore();
+            //result = SaveScore(score, 3);
 
         }
     }
